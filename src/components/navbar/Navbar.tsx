af44abf63d0e51347425803adb2f313626948eb0
Simon Sandvik Lee <simonsandviklee@gmail.com>
2024-10-08 12:16:03 +0200

    feat: Add Navbar

---
+++ src/components/navbar/Navbar.tsx
@@ -0,0 +1,13 @@
+import Image from "next/image";
+import ProfileCard from "../widget/ProfileCard";
+
+const Navbar = () => {
+  return <nav className="flex justify-between h-32 w-full px-16 items-center">
+    <div className="h-22 w-22">
+      <Image src="/logo.svg" alt="fs logo" height={100} width={100} className="object-cover w-full h-full"/>
+    </div>
+    <ProfileCard name="John Doe" title="Software Engineer" image="/assets/props/sandviklee.jpg" />
+  </nav>
+}
+
+export default Navbar;
