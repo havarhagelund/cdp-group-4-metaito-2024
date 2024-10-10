6452c2b10321cb4bcbb09ce3c6d204cf4267f5ff
Simon Sandvik Lee <simonsandviklee@gmail.com>
2024-10-10 09:28:54 +0200

    refactor: Main page to use arrow function

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,28 @@
+"use client";
 import Navbar from "@/components/navbar/Navbar";
+import Content from "@/components/content/Content";
+import Card from "@/components/widget/Card";
 import Image from "next/image";
+import { MousePosition } from "@/types/mouse";
+import { useEffect, useState } from "react";
 
-export default function Home() {
+const Home = () => {
+  const [mousePosition, setMousePosition] = useState<MousePosition>({ x: undefined, y: undefined });
+  const getMousePosition = (ev: any) => {
+    setMousePosition({ x: ev.clientX, y: ev.clientY });
+  };
   return (
     <>
       <Navbar />
-      <main className="px-16 py-2">
-        <section className="font-medium tracking-wide space-y-2 cursor-default">
+      <main className="py-2">
+        <section className="px-16 font-medium tracking-wide space-y-2 cursor-default">
           <p className="text-text-subheader text-xl">Customer Driven Project</p>
           <p className="text-text-header text-4xl">AI Structure Assistant</p>
         </section>
+        <Content/>
       </main>
     </>
   );
 }
+
+export default Home;
