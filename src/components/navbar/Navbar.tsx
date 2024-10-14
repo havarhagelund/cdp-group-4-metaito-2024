f8750322a1b1c0ee71d8c8786b7f241d516c27ca
Simon Sandvik Lee <simonsandviklee@gmail.com>
2024-10-15 00:09:13 +0200

    feat: Update Navbar
    Add draggable false, and run prettier

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -3,14 +3,13 @@ import ProfileCard from "../profile/ProfileCard";
 
 const Navbar = () => {
   return (
-    <nav className="flex justify-between h-32 w-full px-16 items-center">
+    <nav className="flex justify-between h-32 w-full px-16 pt-4 items-center">
       <div className="h-22 w-22">
         <Image
           src="/logo.svg"
           alt="fs logo"
           height={100}
           width={100}
-          draggable={false}
           className="object-cover w-full h-full"
         />
       </div>
