b8cce1b76fa6537730e29a0e144d1c465f5a8ef3
Håvar Hagelund <havar.hagelund@gmail.com>
2024-10-15 10:00:43 +0200

    added route for creating embedding

---
+++ src/utils/create-embedding.ts
@@ -0,0 +1,20 @@
+export async function CreateEmbedding(input: string) {
+  let response = await fetch("https://api.openai.com/v1/embeddings", {
+    method: "POST",
+    body: JSON.stringify({
+      input: input,
+      model: "text-embedding-3-small",
+      userId: 1,
+    }),
+    headers: {
+      "Content-type": "application/json",
+      Authorization: "Bearer " + process.env.OPEN_API_KEY,
+    },
+  });
+
+  const json = await new Response(response.body).json();
+  const embedding = json.data[0].embedding;
+  console.log(embedding);
+
+  return embedding;
+}
