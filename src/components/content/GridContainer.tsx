afe8026a9667918c54b6b0f98bd0c4c224a13da2
Simon Sandvik Lee <simonsandviklee@gmail.com>
2024-10-15 18:05:42 +0200

    refactor: Rewrite GridContainer logic
    Rewrite the whole 2D grid logic from emulating the CSS grid to listening
    for changes and updating the 2D array given the source of truth.
    
    Therefore the need for some of the grid algorithms have been deprecated.

--- src/components/content/GridContainer.tsx
+++ src/components/content/GridContainer.tsx
@@ -1,27 +1,20 @@
 "use client";
 import { FC, useEffect, useRef, useState } from "react";
-import Card from "../widget/Card";
-import { size, direction } from "@/types/layout";
+import Card, { CardRef } from "../widget/Card";
+import { size, card, grid, position } from "@/types/layout";
 import {
-  findLastRow,
   getSizeFromGrid,
-  gridFindFirst,
-  gridPlace,
-  gridRemove,
-  idRowSpanLen,
+  amountIdColumn,
+  amountZeroColumn,
   indexOf2D,
-  remainInColumn,
-  remainInRow,
+  gridPlace,
 } from "@/utils/grid";
 
 const GridContainer: FC = () => {
-  const sectionRef = useRef<HTMLDivElement>(null);
-  const [grid, setGrid] = useState<number[][]>([
-    [1, 2, 3, 4],
-    [1, 2, 5, 4],
-    [6, 6, 6, 0],
-  ]);
+  const gridRef = useRef<HTMLDivElement>(null);
+  const [grid, setGrid] = useState<grid | null>(null);
   const [cardIds] = useState<number[]>([1, 2, 3, 4, 5, 6]);
+  const cardRefs = useRef<CardRef[]>([]);
   const columnCap = 4;
   const rowCap = 3;
 
@@ -29,99 +22,77 @@ const GridContainer: FC = () => {
     setGrid([
       [1, 2, 3, 4],
       [1, 2, 5, 4],
-      [6, 6, 6, 0],
+      [6, 6, 6, 6],
     ]);
   }, []);
 
-  function extendCard(id: number, resizeDirection: direction) {
-    setGrid((prevGrid) => {
-      const { index, row } = indexOf2D(id, grid, rowCap);
-      const lastRow = findLastRow(id, grid, rowCap);
-      switch (resizeDirection) {
-        case "right": {
-          const remRows = remainInRow(row, grid);
-          if (remRows === 0) return prevGrid;
-          for (let i = row; i <= lastRow; i++) {
-            prevGrid[i].splice(index, 0, id);
-            const zeroIndex = prevGrid[i].indexOf(0);
-            prevGrid[i].splice(zeroIndex, 1);
-          }
-          break;
-        }
-        case "bottom": {
-          if (lastRow >= rowCap) return prevGrid;
-          const nextElem = grid[lastRow + 1][index];
-          const currSize = getSizeFromGrid(id, grid, rowCap, columnCap);
-          if (nextElem !== 0) {
-            const nextElemSize = getSizeFromGrid(
-              nextElem,
-              grid,
-              rowCap,
-              columnCap,
-            );
-            gridRemove(nextElem, nextElemSize, grid, rowCap);
-            gridPlace(
-              id,
-              { width: currSize.width, height: currSize.height + 1 },
-              { index, row },
-              grid,
-            );
-            gridPlace(
-              nextElem,
-              nextElemSize,
-              gridFindFirst(nextElemSize, grid, rowCap, columnCap),
-              grid,
-            );
-          } else {
-            gridPlace(
-              id,
-              { width: currSize.width, height: currSize.height + 1 },
-              { index, row },
-              grid,
-            );
-          }
-          break;
-        }
-      }
-      return prevGrid;
-    });
+  function setRefs(ref: CardRef) {
+    if (cardRefs.current.some((el) => el.id === ref.id)) return;
+    cardRefs.current.push(ref);
   }
 
-  function shortenCard(id: number, resizeDirection: direction) {
-    setGrid((prevGrid) => {
-      const { index, row } = indexOf2D(id, grid, rowCap);
-      const lastRow = findLastRow(id, grid, rowCap);
-      switch (resizeDirection) {
-        case "right": {
-          for (let i = row; i <= lastRow; i++) {
-            prevGrid[i].splice(index, 1);
-            prevGrid[i].push(0);
-          }
-          break;
-        }
-        case "bottom": {
-          const lastIndex = prevGrid[row].lastIndexOf(id);
-          for (let i = index; i <= lastIndex; i++) {
-            prevGrid[lastRow][i] = 0;
-          }
-          break;
-        }
-      }
-      return prevGrid;
+  function toGridPosition(pos: position): position {
+    const { width, height } = getMinSize();
+    const gridSize = gridRef.current!.getBoundingClientRect();
+    const x = Math.ceil(Math.abs(pos.x) / (width - 10)) - 1;
+    const y = Math.ceil(Math.abs(pos.y - gridSize.y) / height) - 1;
+    return { x, y };
+  }
+
+  function getCardPosition(id: number): position {
+    const cardRef: CardRef | undefined = cardRefs.current.find(
+      (ref) => ref.id == id,
+    );
+    if (!cardRef) throw new Error("Card not found");
+    const pos = cardRef.ref.current!.getBoundingClientRect();
+    return toGridPosition(pos);
+  }
+
+  function getCardSize(id: number): size {
+    const cardRef: CardRef | undefined = cardRefs.current.find(
+      (ref) => ref.id == id,
+    );
+    if (!cardRef) throw new Error("Card not found");
+    const { width, height } = getMinSize();
+    const client = cardRef.ref.current!.getBoundingClientRect();
+    return {
+      width: Math.ceil(client.width / width),
+      height: Math.ceil(client.height / height),
+    };
+  }
+
+  function getCard(id: number): card {
+    const size = getCardSize(id);
+    const position = getCardPosition(id);
+    return { size, position };
+  }
+
+  function placeCard(id: number, card: card, grid: number[][]) {
+    gridPlace(id, card.size, card.position, grid);
+  }
+
+  function updateGrid() {
+    const newGrid: number[][] = Array.from({ length: rowCap }, () =>
+      Array(columnCap).fill(0),
+    );
+    cardIds.forEach((id) => {
+      placeCard(id, getCard(id), newGrid);
     });
+    setGrid(newGrid);
   }
 
   function getContainerSize(): size {
-    if (!sectionRef.current) return { width: 0, height: 0 };
-    const { width, height } = sectionRef.current.getBoundingClientRect();
+    if (!gridRef.current) throw new Error("Section reference is not set");
+    const { width, height } = gridRef.current.getBoundingClientRect();
     return { width: width, height: height };
   }
 
   function getCurrPossibleCardSize(id: number): size {
-    const { row } = indexOf2D(id, grid, rowCap);
-    const possibleWidth = grid[row].filter((el) => el == 0 || el == id).length;
+    if (!grid) throw new Error("Grid is not set");
+    const { y } = indexOf2D(id, grid, rowCap);
+    const possibleWidth = grid[y].filter((el) => el == 0 || el == id).length;
     const possibleHeight =
-      remainInColumn(id, grid, rowCap) + idRowSpanLen(id, grid, rowCap);
+      amountZeroColumn(id, grid, rowCap) + amountIdColumn(id, grid, rowCap);
     return { width: possibleWidth, height: possibleHeight };
   }
 
@@ -133,21 +104,31 @@ const GridContainer: FC = () => {
     return { width: cardWidth, height: cardHeight };
   }
 
+  if (!grid) {
+    return (
+      <>
+        <p className="w-full text-center flex justify-center items-center">
+          Loading...
+        </p>
+      </>
+    );
+  }
+
   return (
     <section
-      ref={sectionRef}
+      ref={gridRef}
       className="px-16 pt-6 h-[75vh] w-[96vw] grid gap-2 grid-cols-4 grid-rows-3 justify-start content-start"
     >
       {cardIds.map((id) => {
         return (
           <Card
-            key={id}
             id={id}
-            minSize={getMinSize()}
+            key={id}
+            getMinSize={getMinSize}
             startSize={getSizeFromGrid(id, grid, rowCap, columnCap)}
-            extend={extendCard}
-            shorten={shortenCard}
-            getCurrentMaxSize={getCurrPossibleCardSize}
+            update={updateGrid}
+            getCurrentPossibleSize={getCurrPossibleCardSize}
+            setRefs={setRefs}
           />
         );
       })}
