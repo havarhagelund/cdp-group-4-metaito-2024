15aba328ce603f83495db3f5fcc6b5ad32c8c4cc
Simon Sandvik Lee <simonsandviklee@gmail.com>
2024-10-17 13:43:18 +0200

    feat: Add People/Members widget

---
+++ src/components/widget/Member.tsx
@@ -0,0 +1,32 @@
+import { member } from "@/types/splat";
+import ProfileCard from "../profile/ProfileCard";
+import { size } from "@/types/layout";
+
+interface MemberProps {
+  members: member[];
+  currentSize: size;
+}
+
+const Member = ({ members, currentSize }: MemberProps) => {
+  return (
+    <main
+      style={{
+        gridTemplateColumns: `repeat(${currentSize.width}, minmax(0, 1fr))`,
+        gridTemplateRows: `repeat(${currentSize.height}, minmax(0, 1fr))`,
+      }}
+      className="w-full h-full grid gap-4"
+    >
+      {members.map((member: member, index: number) => (
+        <ProfileCard
+          key={index}
+          name={member.name}
+          title={member.role}
+          image={member.image}
+          border={false}
+        />
+      ))}
+    </main>
+  );
+};
+
+export default Member;
