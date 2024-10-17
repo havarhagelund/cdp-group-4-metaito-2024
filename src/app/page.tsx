1dd93c5bebc39ffcc5cf4f3450a47a2d67537630
Simon Sandvik Lee <simonsandviklee@gmail.com>
2024-10-17 13:41:44 +0200

    feat: Add Data & Implement for Splat

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,12 @@
 import Navbar from "@/components/navbar/Navbar";
 import Toolbar from "@/components/toolbar/Toolbar";
 import GridContainer from "@/components/content/GridContainer";
+import { splatData } from "@/data/Splat";
+import { useState } from "react";
+import { splat } from "@/types/splat";
 
 const Home = () => {
+  const [splat] = useState<splat>(splatData);
   return (
     <>
       <Navbar />
@@ -12,11 +16,11 @@ const Home = () => {
           <Toolbar />
         </div>
         <section className="px-16 font-medium tracking-wide space-y-2 cursor-default">
-          <p className="text-text-subheader text-xl">Customer Driven Project</p>
-          <p className="text-text-header text-4xl">AI Structure Assistant</p>
+          <p className="text-text-subheader text-xl">{splat.subtitle}</p>
+          <p className="text-text-header text-4xl">{splat.title}</p>
         </section>
         <section className="flex h-fit w-full">
-          <GridContainer />
+          <GridContainer content={splat.content} grid={splat.grid} />
         </section>
       </main>
     </>
