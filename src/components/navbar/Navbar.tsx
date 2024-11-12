b81e7b0f9e0c2f030b6c5d11cc32008c25b3b3fa
Simon Sandvik Lee <simonsandviklee@gmail.com>
2024-11-12 18:35:15 +0100

    refactor: Add Link to Navbar logo

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,10 +1,11 @@
 import Image from "next/image";
 import ProfileCard from "../profile/ProfileCard";
+import Link from "next/link";
 
 const Navbar = () => {
   return (
     <nav className="flex justify-between h-32 w-full px-16 pt-4 items-center">
-      <div className="h-22 w-22">
+      <Link href="/" className="h-22 w-22">
         <Image
           src="/logo.svg"
           alt="fs logo"
@@ -12,7 +13,7 @@ const Navbar = () => {
           width={100}
           className="object-cover w-full h-full"
         />
-      </div>
+      </Link>
       <ProfileCard
         name="John Doe"
         title="Software Engineer"
