b0e494331b73af9a6ec90277bc599bb9f7f9d0de
Ksenia Mordovets <kmordovets@gmail.com>
2024-11-13 14:50:34 +0100

    feat: add MeshGradientRenderer background on form page

--- src/app/form/page.tsx
+++ src/app/form/page.tsx
@@ -3,11 +3,28 @@
 import React from "react";
 import FormProvider from "@/context/FormContext";
 import FormCard from "@/components/form/FormCard";
+import { MeshGradientRenderer } from '@johnn-e/react-mesh-gradient';
 
 
 const Page = () => {
   return (
     <FormProvider>
+      <MeshGradientRenderer
+                    className="gradient"
+                    colors={[
+                        "#E0FFFF",
+                        "#E1FFFF",
+                        "#BBDEE3",
+                        "#E9F9FB",
+                        "#E9F9FB"
+                    ]}
+                    wireframe={false}
+                    style={{
+                        position: "fixed",
+                        height: "100vh",
+                        width: "100vw",
+                    }}
+                /> 
         <div className="h-screen w-screen flex justify-center items-center">
             <FormCard />
         </div>
