3e840f11dcbe641a54cfb23f58963c48b4aa3088
Simon Sandvik Lee <simonsandviklee@gmail.com>
2024-11-18 09:27:57 +0100

    refactor: Update name of checklist widget & Add placeholder

---
+++ src/components/widget/CheckList.tsx
@@ -0,0 +1,106 @@
+import { useState } from "react";
+import { checkItem } from "@/types/splat";
+import { size } from "@/types/layout";
+import { useSplatStore } from "@/store/Splat";
+import EditChecklistPopup from "./EditChecklistPopup";
+import EditTextPopup from "./EditTextPopup";
+
+interface CheckItemProps {
+  item: checkItem;
+  index: number;
+  handleCheckboxChange: (index: number) => void;
+}
+
+const CheckItem = ({ item, index, handleCheckboxChange }: CheckItemProps) => {
+  return (
+    <main className="w-fit h-22 rounded-2xl">
+      <input
+        id={`checkbox-${index}`}
+        type="checkbox"
+        checked={item.checked}
+        onChange={() => handleCheckboxChange(index)}
+        className="hidden"
+        disabled={item.placeholder}
+      />
+      <label
+        htmlFor={`checkbox-${index}`}
+        className="relative flex items-center cursor-pointer gap-x-4"
+      >
+        <span
+          className={`w-10 h-10 inline-block rounded
+                  ${item.checked && !item.placeholder
+              ? "bg-primary-default"
+              : "bg-gray-100 border-2 border-gray-300"
+            }
+                  transition-all duration-200 ease-in-out`}
+        >
+          {item.checked && !item.placeholder && (
+            <svg
+              className="w-10 h-10 text-white"
+              fill="none"
+              stroke="currentColor"
+              viewBox="0 0 24 24"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M5 13l4 4L19 7"
+              />
+            </svg>
+          )}
+        </span>
+        <p className={`${item.placeholder ? "text-lines-default hover:scale-[1.01] hover:text-gray-800 transition-transform" : "text-black"} text-lg`}>
+          {item.placeholder ? "Add " + item.title : item.title}
+        </p>
+      </label>
+    </main>
+  );
+}
+
+interface ChecklistProps {
+  id: number;
+  currentSize: size;
+  items: checkItem[];
+}
+
+const Checklist = ({ id, items: initialItems, currentSize }: ChecklistProps) => {
+  const { updateStoreDroplets, addStoreDroplet, removeStoreDroplet } = useSplatStore();
+
+  function handleCheckboxChange(index: number) {
+    const newItems = [...initialItems];
+    newItems[index].checked = !newItems[index].checked;
+    updateStoreDroplets(id, newItems);
+  }
+
+  return (
+    <main
+      className="w-full h-full flex flex-col pt-4 space-y-6 items-start"
+    >
+      {initialItems.map((item: checkItem, index: number) => (
+        item.placeholder ? (
+          <EditChecklistPopup key={index} widgetId={id} dropletId={item.id}>
+            <button className="text-start">
+              <CheckItem
+                key={index}
+                item={item}
+                index={index}
+                handleCheckboxChange={handleCheckboxChange}
+              />
+            </button>
+          </EditChecklistPopup>
+        ) : (
+          <CheckItem
+            key={index}
+            item={item}
+            index={index}
+            handleCheckboxChange={handleCheckboxChange}
+          />
+        )
+      ))}
+    </main>
+  );
+};
+
+export default Checklist;
