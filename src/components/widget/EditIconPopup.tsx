e22261a4f5d43399f0be0ddc79a7f060925b9bfe
Simon Sandvik Lee <simonsandviklee@gmail.com>
2024-11-18 17:53:05 +0100

    feat: Add Icon Widget & Popup

---
+++ src/components/widget/EditIconPopup.tsx
@@ -0,0 +1,111 @@
+"use client";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+} from "@/components/ui/form";
+import { Button } from "@/components/ui/button";
+import {
+  Dialog as ShadDialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import { Input } from "@/components/ui/input";
+import { useSplatStore } from "@/store/Splat";
+import { updateSplat } from "@/utils/update-splat";
+import { useState } from "react";
+import { EditIcon } from "lucide-react";
+
+const formSchema = z.object({
+  url: z.string().min(1),
+});
+
+interface EditIconPopupProps {
+  widgetId: number;
+  dropletId: number;
+  children?: React.ReactNode;
+}
+
+const EditIconPopup = ({
+  widgetId,
+  dropletId,
+  children,
+}: EditIconPopupProps) => {
+  const { id, content, grid, addStoreDroplet, removeStoreDroplet } =
+    useSplatStore();
+
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      url: "",
+    },
+  });
+
+  function onSubmit(values: z.infer<typeof formSchema>) {
+    if (!grid || !content) throw new Error("Grid or content is missing");
+    removeStoreDroplet(widgetId, dropletId);
+    addStoreDroplet(widgetId, dropletId, {
+      id: dropletId,
+      url: values.url,
+      placeholder: false,
+    });
+    updateSplat(id, { grid, content });
+  }
+
+  return (
+    <ShadDialog>
+      <DialogTrigger asChild><button>{children}</button></DialogTrigger>
+      <DialogContent className="bg-background-widget py-8 w-5/6 h-fit">
+        <DialogHeader>
+          <DialogTitle className="text-xl">Add Icon Droplet</DialogTitle>
+          <DialogDescription className="text-[14px]">
+            Add an icon to your widget
+          </DialogDescription>
+        </DialogHeader>
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(onSubmit)}>
+            <div className="flex justify-between pt-2 pb-10 w-full">
+              <FormField
+                control={form.control}
+                name="url"
+                render={({ field }) => (
+                  <FormItem className="w-full">
+                    <FormLabel>Url of icon link</FormLabel>
+                    <FormControl>
+                      <Input
+                        type="text"
+                        placeholder="Insert the url of the icon..."
+                        {...field}
+                      />
+                    </FormControl>
+                  </FormItem>
+                )}
+              />
+            </div>
+            <DialogFooter className="py-2">
+              <div className="flex w-full justify-end items-center">
+                <Button
+                  type="submit"
+                  className="bg-primary-second text-md text-white"
+                >
+                  Edit Widget
+                </Button>
+              </div>
+            </DialogFooter>
+          </form>
+        </Form>
+      </DialogContent>
+    </ShadDialog>
+  );
+};
+
+export default EditIconPopup;
